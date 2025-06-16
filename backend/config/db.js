cf4e8701b52d60a8208c3c6152adfa9ac0a7cfe8
akash76111565864 <akashk.it2023@citchennai.net>
2025-06-16 17:10:29 +0530

    backend deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
+//added port and logging
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  port: 40813,
+  logging: false
 });
 
 module.exports = sequelize;
